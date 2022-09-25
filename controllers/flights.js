5fd75b1df7455410a3f5b4d1c61baec5437055dd
Tal <trimoni8@gmail.com>
2022-09-25 13:12:02 -0400

    add edit and show

--- controllers/flights.js
+++ controllers/flights.js
@@ -21,8 +21,10 @@ function newFlights(req, res) {
 }
 
 function create(req, res) {
+  console.log('CREATE FORM', req.body)
   Flight.create(req.body)
   .then(flight => {
+    console.log('NEWLY CREATED FLIGHT', flight)
     res.redirect('/flights')
   })
   .catch(err => {
@@ -31,8 +33,61 @@ function create(req, res) {
   })
 }
 
+function deleteFlights(req, res) {
+  Flight.findByIdAndDelete(req.params.id)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+}
+
+function show (req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render('flights/show', {
+      flight: flight
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+}
+
+function edit(req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render('flights/edit', {
+      flight: flight
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+}
+
+function update(req, res) {
+  req.body.flight = !!req.body.flight
+  Flight.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(flight => {
+    res.redirect(`/flights/${flight._id}`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+}
+
 export {
   index,
   newFlights as new,
   create,
+  deleteFlights as delete,
+  show,
+  edit,
+  update,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -6,7 +6,15 @@ router.get('/', flightsCtrl.index)
 
 router.get('/new', flightsCtrl.new)
 
+router.get('/:id', flightsCtrl.show)
+
+router.get('/:id/edit', flightsCtrl.edit)
+
 router.post('/', flightsCtrl.create)
+
+router.delete('/:id', flightsCtrl.delete)
+
+router.put('/:id', flightsCtrl.update)
 
 export {
   router
